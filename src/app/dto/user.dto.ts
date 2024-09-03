6975ef3c8329e4bff8ffaa1d9eb47d2de2ab7d9f
Leoni <leonimascarenhas@gmail.com>
2024-09-03 19:05:55 -0300

    user dto created

--- src/app/dto/user.dto.ts
+++ src/app/dto/user.dto.ts
@@ -2,8 +2,8 @@ export interface User {
   name: string;
   email: string;
   role: string;
-  isOnboarded: string;
-  createdAt: Date;
-  deletedAt: Date;
-  updatedAt: Date;
+  isOnboarded: number;
+  createdAt?: Date;
+  deletedAt?: Date;
+  updatedAt?: Date;
 }
